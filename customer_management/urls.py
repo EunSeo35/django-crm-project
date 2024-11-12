4c428e892f0646266176ae4c41130e7fdc678fa4
selina <eunbiro@gmail.com>
2024-11-12 16:32:42 +0900

    add 'delete' page

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomerListView
+from .views import CustomerListView, CustomerDeleteView
 
 app_name = 'customer_management'
 
 urlpatterns = [
     path('list/', CustomerListView.as_view(), name='customer_list'),
+
+    path('<int:pk>/delete/', CustomerDeleteView.as_view(), name='customer_delete'),
 ]
--- customer_management/views.py
+++ customer_management/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from .models import Customer, Purchase
 
 class CustomerListView(ListView):
@@ -8,3 +8,9 @@ class CustomerListView(ListView):
     template_name = 'customer_list.html'
     context_object_name = 'customers'
 
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'customer_confirm_delete.html'
+    success_url = reverse_lazy('customer_list')
