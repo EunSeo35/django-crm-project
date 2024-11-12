4af7882a0bc4be361d163cb0cec13f338a96e76c
selina <eunbiro@gmail.com>
2024-11-12 15:45:33 +0900

    add customer_list page

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from config.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", main),
+    path('',main),
+    path('',include('customer_management.urls')),
 ]
--- customer_management/views.py
+++ customer_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from .models import Customer, Purchase
 
-# Create your views here.
+class CustomerListView(ListView):
+    model = Customer
+    template_name = 'customer_list.html'
+    context_object_name = 'customers'
+
