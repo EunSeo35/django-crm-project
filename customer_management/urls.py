fa5b073953999ecd56d3b89f03c8911a9879b340
eunseo <pes1365@naver.com>
2024-11-12 16:23:24 +0900

    Create customer add & make a link customer_list page to customer_form page

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomerListView
+from .views import CustomerListView, CustomerCreateView
 
 app_name = 'customer_management'
 
 urlpatterns = [
     path('list/', CustomerListView.as_view(), name='customer_list'),
+    path('list/add',CustomerCreateView.as_view(), name = 'customer_add' )
 ]
--- customer_management/views.py
+++ customer_management/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Customer, Purchase
 
 class CustomerListView(ListView):
     model = Customer
     template_name = 'customer_list.html'
     context_object_name = 'customers'
-
+    
+class CustomerCreateView(CreateView):
+    model = Customer
+    template_name = 'customer_form.html'
+    fields = ['name', 'email', 'phone', 'address']
+    success_url = reverse_lazy('customer_management:customer_list') # customer_management/urls.py에서 customer_list라는 이름을 가진 URL 패턴을 찾아서 해당 URL을 반환
+ 
