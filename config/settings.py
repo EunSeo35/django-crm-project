a020136b20d3631b25ea2068d5d60052f8534803
selina <eunbiro@gmail.com>
2024-11-12 11:35:20 +0900

    create 'customer_management' app

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'customer_management',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
