7eedef35043c32ae6009bcad6bb2441ce621d457
selina <eunbiro@gmail.com>
2024-11-12 17:43:07 +0900

    updated info

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CustomerListView, CustomerCreateView, CustomerDeleteView
+from .views import CustomerListView, CustomerCreateView, CustomerUpdateView ,CustomerDeleteView
 
 app_name = 'customer_management'
 
 urlpatterns = [
     path('list/', CustomerListView.as_view(), name='customer_list'),
     path('list/add',CustomerCreateView.as_view(), name = 'customer_add' ),
+    path('<int:pk>/edit/',CustomerUpdateView.as_view(), name = 'customer_edit'), #특정 customer의 pk값 
     path('<int:pk>/delete/', CustomerDeleteView.as_view(), name='customer_delete'),
     
 ]
