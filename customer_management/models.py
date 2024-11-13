3687111fe33efba572fb068f95267d7e46aa5ed6
eunseo <pes1365@naver.com>
2024-11-13 16:46:12 +0900

    purchase model 상품카테고리 형식 변경

--- customer_management/models.py
+++ customer_management/models.py
@@ -14,14 +14,28 @@ class Customer(models.Model):
         return f"{self.name}"
 
 class Purchase(models.Model):
+    
+    CATEGORY_CHOICES = [
+        ('1', '카테고리 1'),
+        ('2', '카테고리 2'),
+        ('3', '카테고리 3'),
+        ('4', '카테고리 4'),
+        ('5', '카테고리 5'),
+        ('6', '카테고리 6'),
+        ('7', '카테고리 7'),
+        ('8', '카테고리 8'),
+        ('9', '카테고리 9'),
+        ('10', '카테고리 10'),
+        ('11', '카테고리 11'),
+        ('12', '카테고리 12'),  ]   
+        
     customer = models.ForeignKey(Customer, on_delete = models.CASCADE, related_name = 'purchases')
     product_name = models.CharField(max_length=100)
     product_code = models.CharField(max_length=50,null=True, blank=True)
-    category = models.CharField(max_length=50,null=True, blank=True)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES,null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     
     def __str__(self):
         return f"{self.product_name} - {self.customer.name}"
-    
     
