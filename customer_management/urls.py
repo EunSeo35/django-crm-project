6a240cbdb7bc45fa6d21b9bb348fcd6b5702ea54
selina <eunbiro@gmail.com>
2024-11-13 11:18:44 +0900

    edit stats

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomerListView, CustomerCreateView, CustomerUpdateView ,CustomerDeleteView,CustomerPurchaseDetailView
+from .views import CustomerListView, CustomerCreateView, CustomerUpdateView ,CustomerDeleteView,CustomerPurchaseDetailView, customer_statistics
 
 app_name = 'customer_management'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('list/add',CustomerCreateView.as_view(), name = 'customer_add' ),
     path('<int:pk>/edit/',CustomerUpdateView.as_view(), name = 'customer_edit'), #특정 customer의 pk값 
     path('<int:pk>/delete/', CustomerDeleteView.as_view(), name='customer_delete'),
-    path('<int:pk>/purchase_detail/', CustomerPurchaseDetailView.as_view(), name='customer_purchase_detail')
+    path('<int:pk>/purchase_detail/', CustomerPurchaseDetailView.as_view(), name='customer_purchase_detail'),
+    path('statistics/', customer_statistics, name='customer_statistics'),
 ]
--- customer_management/views.py
+++ customer_management/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView #장고에서 제공 
 from .models import Customer, Purchase
+from django.utils import timezone
+from django.db.models import Sum
+from datetime import timedelta, date
 
 class CustomerListView(ListView):
     model = Customer
@@ -35,4 +38,16 @@ class CustomerPurchaseDetailView(DetailView):
         customer = self.get_object()
         context['purchases'] = Purchase.objects.filter(customer=customer)
         return context
-    
+
+def customer_statistics(request):
+    today = timezone.now().date()
+    last_30_days = today - timedelta(days=30)
+
+    # 지난 30일 동안의 구매 총합
+    recent_purchases = Purchase.objects.filter(date__gte=last_30_days)
+    total_sales = recent_purchases.aggregate(Sum('amount'))['amount__sum'] or 0
+
+    return render(request, 'customer_statistics.html', {
+        'total_sales': total_sales,
+        'recent_purchases': recent_purchases,
+    })
